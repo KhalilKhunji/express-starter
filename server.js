354dbded0a1f18647af2c53fce11383411f322f7
Khalil Khunji <khalilkhunji@gmail.com>
2025-10-18 14:52:37 +0300

    updating some comments

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const morgan = require('morgan');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '3000'; // Setting server port - defaults to 3000 if unspecified in .env
 
 const catsRouter = require('./routes/cats.js');
 
 // Add any imports at the top of the file
 
 // Middleware
-app.use(morgan('dev')); // 
+app.use(morgan('dev')); // For logging HTTP requests
 app.use(express.json()); // For parsing JSON payloads
 
 // Add any additional middleware here (order of middleware matters)
